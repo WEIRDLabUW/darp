9cb9e87952a2dd7d126f2280cc5fd24863b8f204
Quinn Pfeifer <quinnmpfeifer@gmail.com>
2026-06-04 13:04:08 -0700

    Improve install

--- scripts/create_env.sh
+++ scripts/create_env.sh
@@ -1,66 +1,92 @@
-#!/bin/bash
+ROBOSUITE=true
 
-#SBATCH --job-name=knn
-#SBATCH --account=weirdlab
-#SBATCH --partition=gpu-l40s
+if [ "$ROBOSUITE" = "true" ]; then
+  env_name="robosuite"
+else
+  env_name="robocasa"
+fi
 
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
-#SBATCH --gpus-per-task=1
-#SBATCH --mem=20G
-#SBATCH --time=10:00:00
-#SBATCH -o log/%j.out
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=quinnpfeifer@icloud.com
+echo "Creating environment: $env_name"
 
-echo "ID: ${SLURM_JOB_ID}"
-env_name="robosuite"
-#env_name="robocasa"
-source ~/.bashrc
-cd $(dirname $(pwd))
-
-#echo "PRE-DEACTIVATE: ${CONDA_DEFAULT_ENV}"
-conda deactivate
-#conda deactivate
-#echo "PRE-REMOVE: ${CONDA_DEFAULT_ENV}"
-conda env remove --prefix ./envs/$env_name
-#conda clean -a
-#echo "PRE-CREATE: ${CONDA_DEFAULT_ENV}"
 conda env create --prefix ./envs/$env_name -f environment.yml
 conda activate envs/$env_name
 
-#rm -rf $CONDA_PREFIX/lib/python3.10/site-packages/robomimic*
-#rm -rf $CONDA_PREFIX/lib/python3.10/site-packages/robosuite*
-#rm -rf $CONDA_PREFIX/lib/python3.10/site-packages/mimicgen*
-export CUDA_HOME=/mmfs1/sw/cuda/12.3.2/
-#pip install --force-reinstall --no-cache-dir -e GroundingDINO
+# Clone dependencies
+echo "Cloning/Updating dependencies in ./cloned..."
+mkdir -p cloned
 
-# Old robosuite
-pip install -e ../cloned/mimicgen
-pip install pynput==1.6.0
-pip install -e ../cloned/robosuite
-pip install -e ../cloned/robomimic
-pip install mujoco==2.3.2
-pip install numba==0.58.1
+if [ "$ROBOSUITE" = "true" ]; then
+    # Standalone dependencies (for robosuite environment)
+    # mimicgen
+    if [ ! -d "cloned/mimicgen" ]; then
+        git clone https://github.com/NVlabs/mimicgen.git cloned/mimicgen
+        git -C cloned/mimicgen checkout 70048f1d0ec3aa1ab3420fd573526ebeb10cbced
+    fi
 
-# New for casa
-#pip install -e ../cloned/robocasa/robosuite_casa/robosuite/
-#pip install -e ../cloned/robocasa/
-#pip install -e ../cloned/robocasa/mimicgen_casa/
-#pip install -e ../cloned/robocasa/robomimic_casa/
+    # robosuite
+    if [ ! -d "cloned/robosuite" ]; then
+        git clone https://github.com/ARISE-Initiative/robosuite.git cloned/robosuite
+        git -C cloned/robosuite checkout b9d8d3de5e3dfd1724f4a0e6555246c460407daa
+    fi
 
-pip install optuna
-pip install -e ../cloned/r3m
-pip install line_profiler
+    # robomimic
+    if [ ! -d "cloned/robomimic" ]; then
+        git clone https://github.com/ARISE-Initiative/robomimic.git cloned/robomimic
+        git -C cloned/robomimic checkout d0b37cf214bd24fb590d182edb6384333f67b661
+    fi
+else
+    # Robocasa setup (contains its own versions of robosuite, mimicgen, robomimic)
+    if [ ! -d "cloned/robocasa" ]; then
+        git clone https://github.com/robocasa/robocasa.git cloned/robocasa
+        git -C cloned/robocasa checkout 0eae0634a61ad2be33962c9de7000a2dd1ee573f
+    fi
+
+    # Robosuite for Casa
+    if [ ! -d "cloned/robocasa/robosuite_casa/robosuite" ]; then
+        mkdir -p cloned/robocasa/robosuite_casa
+        git clone https://github.com/ARISE-Initiative/robosuite cloned/robocasa/robosuite_casa/robosuite
+        git -C cloned/robocasa/robosuite_casa/robosuite checkout cb25aae7cac84c10409b96827a6c8d5a21f48f3a
+    fi
+
+    # Mimicgen for Casa
+    if [ ! -d "cloned/robocasa/mimicgen_casa" ]; then
+        git clone https://github.com/NVlabs/mimicgen cloned/robocasa/mimicgen_casa
+        git -C cloned/robocasa/mimicgen_casa checkout 36ef5b744eeac684e3e5c2809f403177b8fc15e3
+    fi
+
+    # Robomimic for Casa
+    if [ ! -d "cloned/robocasa/robomimic_casa" ]; then
+        git clone https://github.com/ARISE-Initiative/robomimic cloned/robocasa/robomimic_casa
+        git -C cloned/robocasa/robomimic_casa checkout 271a76c2d55c8b0f94d3d589f26fcae0d47f64a1
+    fi
+fi
+
+# R3M (Common)
+if [ ! -d "cloned/r3m" ]; then
+    git clone git@github.com:facebookresearch/r3m.git cloned/r3m
+    git -C cloned/r3m checkout b2334e726887fa0206962d7984c69c5fb09cceab
+fi
+
+echo "Installing packages for $env_name..."
+
+if [ "$ROBOSUITE" = "true" ]; then
+  # Standard Robosuite Setup
+  pip install -e cloned/mimicgen
+  pip install pynput==1.6.0
+  pip install -e cloned/robosuite
+  pip install -e cloned/robomimic
+  pip install mujoco==2.3.2
+  pip install numba==0.58.1
+else
+  # Robocasa Setup (uses internal versions of dependencies)
+  # Order matters: install dependencies before robocasa itself
+  pip install -e cloned/robocasa/robosuite_casa/robosuite/
+  pip install -e cloned/robocasa/mimicgen_casa/
+  pip install -e cloned/robocasa/robomimic_casa/
+  pip install -e cloned/robocasa/
+fi
+
+# Common packages
+pip install -e cloned/r3m
 pip install --upgrade protobuf
-
-#git clone git@github.com:RoboTwin-Platform/RoboTwin.git
-#export CUDA_HOME=/mmfs1/sw/cuda/12.3.2
-#module load gcc/11.2.0
-#./RoboTwin/script/_install.sh
-
-
-ln -s $CONDA_PREFIX/lib/libEGL.so.1 $CONDA_PREFIX/lib/libEGL.so
-mkdir -p $CONDA_PREFIX/include/X11
-$CONDA_PREFIX/lib/python3.10/site-packages/egl_probe/glad/X11/*.h $CONDA_PREFIX/include/X11/
+pip install optuna line_profiler
